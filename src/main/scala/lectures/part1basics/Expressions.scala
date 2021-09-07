df530d9837458811e54637b62709dcb2df254250
Phi <aiwavision@protonmail.com>
2021-09-06 22:41:52 -0300

    more files

---
+++ src/main/scala/lectures/part1basics/Expressions.scala
@@ -0,0 +1,26 @@
+package lectures.part1basics
+
+object Expressions{
+  @main def main: Unit =
+    val x = 1 + 2 // expression
+    println(x)
+    println(2 + 3 * 4)
+    // + - * / & | ^ << >> >>> (rght shift with zero extension)
+    
+    println(1 == x)
+    // == != > >= < <=
+
+    println(!(1 == x))
+    // ! && ||
+
+    var aVariable = 2
+    aVariable += 3 // also works with -= *= /= (side effects)
+    println(aVariable)
+
+    val aCondition: Boolean = true
+    val aConditionedValue = if aCondition then 5 else 3 // if expression
+    println(aConditionedValue)
+
+    val aWeirdValue = (aVariable = 3)
+    println(aWeirdValue)
+  }
--- src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -1,22 +1,23 @@
 package lectures.part1basics
 
-object ValuesVariablesTypes extends App {
-  // val x: Int = 42
-  // type inference
-  val x = 42
-  println(x)
+object ValuesVariablesTypes{
+  def main: Unit =
+    // val x: Int = 42
+    // type inference
+    val x = 42
+    println(x)
 
-  val aString: String = "Hello"
-  val aChar: Char = 'a'
-  val anInt: Int = x
-  val aShort: Short = 4613
-  val aLong: Long = 918237891323124L
-  val aFloat: Float = 2.0f
-  val aDouble: Double = 3.141592
+    val aString: String = "Hello"
+    val aChar: Char = 'a'
+    val anInt: Int = x
+    val aShort: Short = 4613
+    val aLong: Long = 918237891323124L
+    val aFloat: Float = 2.0f
+    val aDouble: Double = 3.141592
 
-  // variables
-  var aVariable: Int = 4
-  println(aVariable)
-  aVariable = 5
-  println(aVariable)
-}
+    // variables
+    var aVariable: Int = 4
+    println(aVariable)
+    aVariable = 5
+    println(aVariable)
+  }
