15f3106e8878e1aac240d795ad5300537039d61f
Phi <aiwavision@protonmail.com>
2021-10-25 17:01:20 -0300

    finished this class exercises

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -1,17 +1,26 @@
 package lectures.part2oop
 
+import scala.compiletime.ops.boolean
+
 object OOBasics {
   @main def main(): Unit =
     val aPerson = Person("Agatha", 22)
     aPerson.greet("Luna")
     val w = Writer("Agatha", "Lenz", 1999)
+    val imposter = Writer("Agatha", "Lenz", 1999)
     println(w.fullName)
     val n = Novel("Gay Stuff", 2021, w)
     println(n.authorAge)
-    println(n.isWrittenBy(w))
+    println(n.isWrittenBy(imposter))
     val newNovel = n.copy(2022)
     println(newNovel.name)
     println(n == newNovel)
+    val counter = Counter(0)
+    println(counter.c)
+    val newCounter = counter.inc(5)
+    println(newCounter.c)
+    val newNewCounter = newCounter.dec(3)
+    println(newNewCounter.c)
 
 }
 
@@ -36,8 +45,16 @@ class Novel(val name: String, val yearOfRelease: Int, author: Writer){
 }
 
 class Counter(val c: Int){
-  def inc() = Counter(c + 1)
-  def inc(n: Int) = Counter(c + n)
-  def dec() = Counter(c - 1)
-  def dec(n: Int) = Counter(c - n)
+  def inc() = {
+    println("Incrementing.")
+    Counter(c + 1)
+  }
+  def inc(n: Int): Counter = {
+    if (n <= 0) then this else inc().inc(n - 1)
+  }
+  def dec() = {
+    println("Decrementing.")
+    Counter(c - 1)
+  }
+  def dec(n: Int): Counter = if (n <= 0) then this else dec().dec(n - 1)
 }
